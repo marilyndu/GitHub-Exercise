f918423cb96cb50ca3a34ed0be0ec55274b400b5
Marilyn Du <dumf@bc.edu>
2022-09-05 15:02:32 -0400

    Initial Commit

---
+++ GitHub Exercise/GitHub_ExerciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHub_ExerciseApp.swift
+//  GitHub Exercise
+//
+//  Created by Marilyn Du on 9/5/22.
+//
+
+import SwiftUI
+
+@main
+struct GitHub_ExerciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
